cbe4c81f47c8c477223d641134de0c6c89906f58
Aaron Olivieri <aaron.olivieri@vandewiele.com>
2022-08-09 10:53:57 +0200

    Initial Commit

---
+++ HelloWorldTest1/HelloWorldTest1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    class Testprogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world! Write something below: ");
+            string msg = Console.ReadLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Your input: " + msg);
+        }
+    }
+}
