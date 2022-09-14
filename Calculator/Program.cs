a834bcd24deb184f32b79305489b38f9cfc7b6cd
aaron-olivieri-loepfe <aaron.olivieri@vandewiele.com>
2022-09-14 08:42:48 +0200

    Unit Testing works
    Configurated unit testing and added some functionality to program (calc options, rounding of numbers, etc.)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,9 +13,7 @@ namespace CommandCalculator
             bool endApp = false;
 
             while (!endApp)
-            {
-                var line = new String('-', 40);
-
+            {                
                 var args = Console.ReadLine().Split();
                 Start(args);
 
@@ -84,7 +82,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing Addition Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.Addition(args.num1,args.num2);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine(args.num1 + " + " + args.num2 + " = " + sum);
+                Console.WriteLine("------------------------------");
 
             }
             //process SubtractionOptions
@@ -93,7 +93,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing Subtraction Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.Subtraction(args.num1, args.num2);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine(args.num1 + " - " + args.num2 + " = " + sum);
+                Console.WriteLine("------------------------------");
 
             }
             //process MultiplicationOptions
@@ -102,7 +104,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing Multiplication Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.Multiplication(args.num1, args.num2);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine(args.num1 + " * " + args.num2 + " = " + sum);
+                Console.WriteLine("------------------------------");
 
             }
             //process DivisionOptions
@@ -111,7 +115,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing Division Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.Division(args.num1, args.num2);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine(args.num1 + " / " + args.num2 + " = " + sum);
+                Console.WriteLine("------------------------------");
 
             }
             //process SquareRootOptions
@@ -120,7 +126,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing SquareRoot Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.SquareRoot(args.num1);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine("Square Root of " + args.num1 + " is " + sum);
+                Console.WriteLine("------------------------------");
 
             }
             //process PerimeterOptions
@@ -129,7 +137,9 @@ namespace CommandCalculator
                 Console.WriteLine("Processing Perimeter Calculation");
                 MathLibrary mathLibrary = new MathLibrary();
                 double sum = mathLibrary.Perimeter(args.num1);
+                Console.WriteLine("------------------------------");
                 Console.WriteLine("The Perimeter of radius " + args.num1 + " is " + sum);
+                Console.WriteLine("------------------------------");
 
             }
         }
@@ -143,79 +153,56 @@ namespace CommandCalculator
     [Verb("add", HelpText = "Adds two numbers together and shows sum.")]
     internal class AdditionOptions
     {
-
         [Value(1)]
         public double num1 { get; set; }
 
         [Value(2)]
         public double num2 { get; set; }
-
     }
 
-    [Verb("sub", HelpText = "Subtracts two numbers together and shows sum.")]
+    [Verb("sub", HelpText = "Subtracts two numbers and shows sum.")]
     internal class SubtractionOptions
     {
-
         [Value(1)]
         public double num1 { get; set; }
 
         [Value(2)]
         public double num2 { get; set; }
-
     }
 
-    [Verb("mul", HelpText = "Multiplicates two numbers together and shows sum.")]
+    [Verb("mul", HelpText = "Multiplicates two numbers and shows sum.")]
     internal class MultiplicationOptions
     {
-
         [Value(1)]
         public double num1 { get; set; }
 
         [Value(2)]
         public double num2 { get; set; }
-
     }
 
-    [Verb("div", HelpText = "Divides two numbers together and shows sum.")]
+    [Verb("div", HelpText = "Divides two numbers and shows sum.")]
     internal class DivisionOptions
     {
-
         [Value(1)]
         public double num1 { get; set; }
 
         [Value(2)]
         public double num2 { get; set; }
-
     }
 
     [Verb("sqrt", HelpText = "Calculates the Square Root of the entered number.")]
     internal class SquareRootOptions
     {
-
         [Value(1)]
         public double num1 { get; set; }
-
     }
 
     [Verb("peri", HelpText = "Calculates the Perimeter of the entered radius.")]
     internal class PerimeterOptions
     {
-
         [Value(1)]
-        public double num1 { get; set; }
-
+        public double num1 { get; set; }    
     }
-
-    class HelpConstant
-    {
-        public const string Clone_local = "When the repository to clone from is on a local machine, this flag bypasses the normal \"Git aware\" transport mechanism and clones the repository by making a copy of HEAD and everything under objects and refs directories.";
-        public const string Clone_quiet = "Operate quietly. Progress is not reported to the standard error stream.";
-        public const string Clone_repository = "The (possibly remote) repository to clone from. See the GIT URLS section below for more information on specifying repositories.";
-
-        //commit
-        public const string Commit_message = "Use the given <msg> as the commit message. If multiple -m options are given, their values are concatenated as separate paragraphs.";
-    }
-
 
     [AttributeUsage(AttributeTargets.All)]
     class CustomAttribute : Attribute { }
--- MathLibrary/MathLibrary.cs
+++ MathLibrary/MathLibrary.cs
@@ -6,32 +6,32 @@ namespace Calculator
 	{
 		public double Addition(double num1, double num2)
 		{
-			return (num1 + num2);
+			return Math.Round((num1 + num2), 3);
 		}
 
 		public double Subtraction(double num1, double num2)
 		{
-			return (num1 - num2);
+			return Math.Round((num1 - num2), 3);
 		}
 
 		public double Multiplication(double num1, double num2)
 		{
-			return (num1 * num2);
+			return Math.Round((num1 * num2), 3);
 		}
 
 		public double Division(double num1, double num2)
 		{
-			return (num1 / num2);
+			return Math.Round((num1 / num2), 3);
 		}
 
 		public double SquareRoot(double num1)
 		{
-			return (Math.Sqrt(num1));
+			return Math.Round((Math.Sqrt(num1)), 3);
 		}
 
 		public double Perimeter(double num1)
 		{
-			return (2 * Math.PI * num1);
+			return Math.Round((2 * Math.PI * num1), 3);
 		}
 	}
 }
